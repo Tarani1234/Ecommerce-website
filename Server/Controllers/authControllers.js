9f878a1648038913e6fd61778363749779f0c7ec
Tarani1234 <tmaliyala@techdeets.net>
2025-07-10 17:42:25 +0530

    login and signup is completed

---
+++ Server/Controllers/authControllers.js
@@ -0,0 +1,38 @@
+import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js";
+import bcrypt from 'bcryptjs'
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "Email is already exist" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newuser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const token = generateToken(newuser._id);
+    res.status(201).json({ user: newuser, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const Login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "user not found" });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
+    const token = generateToken(user._id);
+    res.json({ user, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
--- Server/server.js
+++ Server/server.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from 'dotenv';
+import connectDB from './Config/db.js'
+import authRoutes from './routes/authroutes.js'
+dotenv.config();
 
+const app = express();
+  connectDB();
 
-mongoose.connect("mongodb+srv://taraniyadav0309:Tarani1234@cluster0.bys9iv2.mongodb.net/ ECOMMERECE_WEBSITE")
- .then(()=> console.log("monodb is connected"))
-   .catch((error) => console.log(error))
-
-
-const app  = express();
-
-app.use( cors({
-    origin : 'http://localhost:5173/',
-    methods : ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders : [
-        "content-Type",
-        "Authorization",
-        "Cache-control",
-        'Expires',
-        'Pragram'
-    ],
-    credentials : true
-}))
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Cache-Control",
+    "Expires",
+    "Pragma"
+  ],
+  credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 
+app.use('/api/auth', authRoutes);
+// Server
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, ()=> console.log(`server is running on the port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
